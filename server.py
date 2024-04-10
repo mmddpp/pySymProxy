f3904f31158c8696e5bba2bc75c984a8e4b6ea7d
mmddpp <24725976+mmddpp@users.noreply.github.com>
2024-04-10 15:15:30 +0800

    Add a divider(===) to distinguish service startup

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from waitress import serve
 import logging
 
 logger = logging.getLogger(__name__)
+logger.info("========================")
 logger.info("Starting SymProxy Server")
 print("Test link: http://localhost:8080/symbols/wntdll.pdb/F999943DF7FB4B8EB6D99F2B047BC3101/wntdll.pdb")
 serve(api, host='0.0.0.0', port=8080)
